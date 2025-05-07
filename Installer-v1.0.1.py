7d930f08c4cc13e1d6893ddb0bf20986738bf5f1
MCAI14 <141061023+MCAI14@users.noreply.github.com>
2025-05-07 09:59:08 +0100

    Create Installer-v1.0.1.py

---
+++ Installer-v1.0.1.py
@@ -0,0 +1,13 @@
+def exibir_boas_vindas():
+    print("========================")
+    print("Bem-vindo ao Pixleaner!")
+    print("========================")
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    exibir_boas_vindas()
