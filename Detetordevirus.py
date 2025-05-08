ab50734ec8f62e5fd49b7156267e32794d7543d2
PixelCorporation24 <pixelcorporation24@outlook.com>
2025-05-08 16:45:58 +0100

    Add initial implementation of virus hash detection

---
+++ Detetordevirus.py
@@ -0,0 +1,14 @@
+virus_hashes = {
+    "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855": "EICAR Test File",
+
+}
+
+import hashlib
+
+def calcular_hash(arquivo):
+    "Calcula o hash de um arquivo"
+    hasher = hashlib.md5()
+    with open(arquivo, "rb") as f:
+        while chunk := f.read(4000):
+            hasher.update(chunk)
+    return hasher.hexdigest()
