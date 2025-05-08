16f2a6398e3a26c26c6b282b1bc3328ca1bee3aa
PixelCorporation24 <pixelcorporation24@outlook.com>
2025-05-08 16:47:06 +0100

    Increase chunk size for file reading in hash calculation

--- Detetordevirus.py
+++ Detetordevirus.py
@@ -9,6 +9,6 @@ def calcular_hash(arquivo):
     "Calcula o hash de um arquivo"
     hasher = hashlib.md5()
     with open(arquivo, "rb") as f:
-        while chunk := f.read(4000):
+        while chunk := f.read(4096):
             hasher.update(chunk)
     return hasher.hexdigest()
